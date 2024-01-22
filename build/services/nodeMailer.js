6efca9432ef59dac3742f7ab616c1ffb21985292
Ademir <ademir@debian.local>
2024-01-22 09:48:00 -0300

    up code

--- build/services/nodeMailer.js
+++ build/services/nodeMailer.js
@@ -15,34 +15,67 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.HandleService = void 0;
 const nodemailer_1 = __importDefault(require("nodemailer"));
 require('dotenv').config();
-// var smtpTransport = require('nodemailer-smtp-transport');
+var smtpTransport = require('nodemailer-smtp-transport');
 const user_email = process.env.USER_EMAIL;
 const pass_email = process.env.PASS_EMAIL;
 class HandleService {
     setSendMail(name, email, phone, comments) {
         return __awaiter(this, void 0, void 0, function* () {
-            let transporter = nodemailer_1.default.createTransport({
-                service: "hotmail",
+            // let transporter = nodemailer.createTransport({
+            //     name: 'no-reply@outlook.com',
+            //     service: "hotmail",
+            //     host: "smtp-mail.outlook.com",
+            //     port: 25,
+            //     // secure: false,
+            //     auth: {
+            //         user: user_email,
+            //         pass: pass_email
+            //     },
+            // });
+            // await transporter.sendMail({
+            //     from: "Ademir <ademir_gre@hotmail.com>",
+            //     to:"ademir_gre@hotmail.com",
+            //     cc:"centroserra@gmail.com," + email,
+            //     subject: "Contato: centroinfo.com.br",
+            //     // text:"Assunto; " + comments,
+            //     html:"<br><b>Sua mensagem para:</b> <a>centroinfo.com.br </a><br><br>"
+            //     + comments
+            // }).then((message: any) => {
+            //     console.log(message)
+            // }).catch((err: any) => {
+            //     console.log(err)
+            // })
+            const smtpConfig = smtpTransport({
                 host: "smtp-mail.outlook.com",
-                port: 25,
-                // secure: false,
+                port: 587,
+                ignoreTLS: false,
+                secure: false,
+                tls: {
+                    rejectUnauthorized: true
+                },
                 auth: {
                     user: user_email,
                     pass: pass_email
-                },
+                }
             });
-            yield transporter.sendMail({
+            const transporter = nodemailer_1.default.createTransport(smtpConfig);
+            const message = {
                 from: "Ademir <ademir_gre@hotmail.com>",
                 to: "ademir_gre@hotmail.com",
-                cc: "centroserra@gmail.com," + email,
-                subject: "Contato: centroinfo.com.br",
-                // text:"Assunto; " + comments,
-                html: "<br><b>Sua mensagem para:</b> <a>centroinfo.com.br </a><br><br>"
-                    + comments
-            }).then((message) => {
-                console.log(message);
-            }).catch((err) => {
-                console.log(err);
+                subject: "Assunto do Email",
+                text: "Conteúdo do email em texto",
+                html: "<h1>Conteúdo do email em HTML</h1>",
+                headers: {
+                    'X-Laziness-level': 1000
+                }
+            };
+            transporter.sendMail(message, function (error, info) {
+                if (error) {
+                    console.log(error);
+                }
+                else {
+                    console.log('Email enviado ' + info.response);
+                }
             });
         });
     }
--- usr/services/nodeMailer.ts
+++ usr/services/nodeMailer.ts
@@ -1,6 +1,6 @@
 import nodemailer from "nodemailer";
 require('dotenv').config()
-// var smtpTransport = require('nodemailer-smtp-transport');
+var smtpTransport = require('nodemailer-smtp-transport');
 
 const user_email = process.env.USER_EMAIL
 const pass_email = process.env.PASS_EMAIL
@@ -8,28 +8,62 @@ const pass_email = process.env.PASS_EMAIL
 export class HandleService {
 
     async setSendMail(name: string, email: string, phone: string, comments: string) {
-        let transporter = nodemailer.createTransport({
-            service: "hotmail",
+        // let transporter = nodemailer.createTransport({
+        //     name: 'no-reply@outlook.com',
+        //     service: "hotmail",
+        //     host: "smtp-mail.outlook.com",
+        //     port: 25,
+        //     // secure: false,
+        //     auth: {
+        //         user: user_email,
+        //         pass: pass_email
+        //     },
+        // });
+        // await transporter.sendMail({
+        //     from: "Ademir <ademir_gre@hotmail.com>",
+        //     to:"ademir_gre@hotmail.com",
+        //     cc:"centroserra@gmail.com," + email,
+        //     subject: "Contato: centroinfo.com.br",
+        //     // text:"Assunto; " + comments,
+        //     html:"<br><b>Sua mensagem para:</b> <a>centroinfo.com.br </a><br><br>"
+        //     + comments
+        // }).then((message: any) => {
+        //     console.log(message)
+        // }).catch((err: any) => {
+        //     console.log(err)
+        // })
+        const smtpConfig = smtpTransport({
             host: "smtp-mail.outlook.com",
-            port: 25,
-            // secure: false,
+            port: 587,
+            ignoreTLS: false,
+            secure : false,
+            tls: {
+                rejectUnauthorized: true
+            },
             auth: {
                 user: user_email,
                 pass: pass_email
-            },
+            }
         });
-        await transporter.sendMail({
+        
+        const transporter = nodemailer.createTransport(smtpConfig);
+        
+        const message:any  = {
             from: "Ademir <ademir_gre@hotmail.com>",
             to:"ademir_gre@hotmail.com",
-            cc:"centroserra@gmail.com," + email,
-            subject: "Contato: centroinfo.com.br",
-            // text:"Assunto; " + comments,
-            html:"<br><b>Sua mensagem para:</b> <a>centroinfo.com.br </a><br><br>"
-            + comments
-        }).then((message: any) => {
-            console.log(message)
-        }).catch((err: any) => {
-            console.log(err)
-        })
+            subject: "Assunto do Email",
+            text: "Conteúdo do email em texto",
+            html: "<h1>Conteúdo do email em HTML</h1>",
+            headers: {
+                'X-Laziness-level': 1000
+            }
+        };
+        transporter.sendMail(message, function(error, info) {               
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email enviado ' + info.response);
+            }
+        });
     }
 }

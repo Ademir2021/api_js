15c017c4732b17b3a552a43c426e457f63526d6b
Ademir <ademir@debian.local>
2024-01-22 15:57:19 -0300

    up code

--- usr/services/nodeMailer.ts
+++ usr/services/nodeMailer.ts
@@ -8,56 +8,27 @@ const user_email = process.env.USER_EMAIL
 const pass_email = process.env.PASS_EMAIL
 
 export class HandleService {
-
     async setSendMail(name: string, email: string, phone: string, comments: string) {
-        // let transporter = nodemailer.createTransport({
-        //     name: 'no-reply@outlook.com',
-        //     service: "hotmail",
-        //     host: "smtp-mail.outlook.com",
-        //     port: 25,
-        //     // secure: false,
-        //     auth: {
-        //         user: user_email,
-        //         pass: pass_email
-        //     },
-        // });
-        // await transporter.sendMail({
-        //     from: "Ademir <ademir_gre@hotmail.com>",
-        //     to:"ademir_gre@hotmail.com",
-        //     cc:"centroserra@gmail.com," + email,
-        //     subject: "Contato: centroinfo.com.br",
-        //     // text:"Assunto; " + comments,
-        //     html:"<br><b>Sua mensagem para:</b> <a>centroinfo.com.br </a><br><br>"
-        //     + comments
-        // }).then((message: any) => {
-        //     console.log(message)
-        // }).catch((err: any) => {
-        //     console.log(err)
-        // })
         const smtpConfig = smtpTransport({
-            // service:"outlook",
+            service: "gmail",
             host: host_email,
             port: port_email,
-            // ignoreTLS: false,
-            // requireTLS: true,
-            secure: false,
-            // tls: {
-            //     rejectUnauthorized: true
-            // },
+            secure: true,
             auth: {
                 user: user_email,
                 pass: pass_email
             }
         });
-
         const transporter = nodemailer.createTransport(smtpConfig);
-
         const message: any = {
-            from: "Ademir <centroserra@gmail.com>",
-            to: "centroserra@gmail.com",
-            subject: "Contato de cliente",
-            html: "<h1>Mensagem recebida de centroinfo.com.br/contact</h1>",
-            text: "Nome Cliente: " + name + "\n" + "Email: " + email + "\n" + "Telefone: " + phone + "\n" + "Assuntos: " + comments,
+            from: "Centro Informática<centroserra@gmail.com>",
+            to: "centroserra@gmail.com," + email,
+            subject: "Contato do Formulário on-line de clientes",
+            html: "<b>Mensagem de:</b> " +
+                "<br>" + "<b>Cliente:</b> " + name +
+                "<br>" + "<b>Email:</b> " + email +
+                "<br>" + "<b>Telefone:</b> " + phone +
+                "<br><br>" + "<b>Assunto:</b> " + comments,
             headers: {
                 'X-Laziness-level': 1000
             }
